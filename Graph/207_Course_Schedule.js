cdfa34754a6ac1324ad77c6aae6405f53f26d788
Yevhenii Chuprun <chuprun.ievgenii@gmail.com>
2023-08-20 18:46:06 +0200

    207_Course_Schedule - polished

--- Graph/207_Course_Schedule.js
+++ Graph/207_Course_Schedule.js
@@ -12,6 +12,8 @@
  * Output: false
  * Explanation: There are a total of 2 courses to take. 
  * To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+ * 
+ * Example 3: 5, [[1,4],[2,4],[3,1],[3,2]
  */
 
 
@@ -50,9 +52,6 @@ function canFinish(numCourses, prerequisites) {
         }
     }
 
-    return !indegree.some(x => x !== 0);
+    return numCourses === 0;
 
 };
-
-
-console.log(canFinish(5, [[1,4],[2,4],[3,1],[3,2]]));
