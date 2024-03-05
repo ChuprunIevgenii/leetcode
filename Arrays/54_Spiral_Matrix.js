6fadbc6508034733cd4e18b983056e86216a7545
Yevhenii Chuprun <chuprun.ievgenii@gmail.com>
2024-03-05 16:17:04 +0100

    54_Spiral_Matrix - polished up

--- Arrays/54_Spiral_Matrix.js
+++ Arrays/54_Spiral_Matrix.js
@@ -29,7 +29,6 @@ function spiralOrder(matrix) {
         }
         top +=1;
 
-
         for(let i = top; i <= bottom; i++) {
             result.push(matrix[i][right]);
         }
@@ -53,7 +52,3 @@ function spiralOrder(matrix) {
 
     return result; 
 };
-
-let matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
-
-console.log(spiralOrder(matrix));
