4643a0fb17c9dfd5da5ffc92fb261b82a980ce79
Yevhenii Chuprun <chuprun.ievgenii@gmail.com>
2024-03-07 16:12:19 +0100

    73_Set_Matrix_Zeroes - solved

---
+++ Arrays/73_Set_Matrix_Zeroes.js
@@ -0,0 +1,42 @@
+//https://leetcode.com/problems/set-matrix-zeroes/description/
+
+//Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+//You must do it in place.
+
+//Example 1:
+
+
+//Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+//Output: [[1,0,1],[0,0,0],[1,0,1]]
+//Example 2:
+
+
+//Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+//Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+
+
+function setZeroes(matrix) {
+    let rows = new Set();
+    let cols = new Set();
+    
+    for(let r = 0; r < matrix.length; r++) {
+        for(let c = 0; c < matrix[0].length; c++) {
+            if(matrix[r][c] === 0) {
+                rows.add(r);
+                cols.add(c);
+            }
+        }
+    }
+
+    for(let r = 0; r < matrix.length; r++) {
+        for(let c = 0; c < matrix[0].length; c++) {
+            if(rows.has(r) ||  cols.has(c)) {
+                matrix[r][c] = 0
+            }
+        }
+    }
+
+
+
+    return matrix;
+};
